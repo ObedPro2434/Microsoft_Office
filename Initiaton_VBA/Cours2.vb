391932a66940c8bf7b016f55b6bbd5a12bae4be4
ObedInfor <kpogok2434@yahoo.com>
2024-10-13 18:12:54 +0100

    Condition

---
+++ Initiaton_VBA/Cours2.vb
@@ -0,0 +1,12 @@
+sub annee()
+	dim age as integer
+	dim calcul as Boolean
+	dim annee_test as Integer
+
+	age = 36
+	annee_test = 2000
+
+	calcul = (2000 - age) >= annee_test
+
+	msgbox (calcul)
+end sub
